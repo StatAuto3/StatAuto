539596d884fef3a56698841938fbea0e463e7b62
zHatsuharu <zhatsuharu@gmail.com>
2025-06-13 13:27:05 +0200

    feat: template login + register

--- apps/app/config/site.ts
+++ apps/app/config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
+  name: "StatAuto",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
@@ -25,40 +25,16 @@ export const siteConfig = {
       href: "/about",
     },
   ],
-  navMenuItems: [
-    {
-      label: "Profile",
-      href: "/profile",
+  auth: {
+    login: {
+      label: "Login",
+      href: "/login",
     },
-    {
-      label: "Dashboard",
-      href: "/dashboard",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
-    },
-  ],
+    register: {
+      label: "Register",
+      href: "/register",
+    }
+  },
   links: {
     github: "https://github.com/heroui-inc/heroui",
     twitter: "https://twitter.com/hero_ui",
