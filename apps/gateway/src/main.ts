185c50910dad0865dc9f1d8cd7745a8f6d445563
Graig <graig.kolodziejczyk@icloud.com>
2025-06-23 22:05:44 +0200

    pilote_service: add swagger git add .!

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  // Configuration Swagger
+  const config = new DocumentBuilder()
+    .setTitle('StatAuto API Gateway')
+    .setDescription(
+      'API Gateway pour la gestion des écuries, pilotes et courses',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4001);
 }
 bootstrap();
